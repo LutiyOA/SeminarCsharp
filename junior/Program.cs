70d489a3801c1b48fc7ad61a6171527b779c5828
Lutiy Oleg <lutiy.oleg@yandex.ru>
2021-11-13 21:14:56 +0700

    Add intern, junior, middle

---
+++ junior/Program.cs
@@ -0,0 +1,127 @@
+﻿/*
+Почувствуй себя джуном
+15. Дано число. Проверить кратно ли оно 7 и 23
+16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным 
+17. По двум заданным числам проверять является ли одно квадратом другого
+18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+19. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+20. Задать номер четверти, показать диапазоны для возможных координат
+21. Программа проверяет пятизначное число на палиндромом.
+22. Найти расстояние между точками в пространстве 2D/3D
+
+Вот задача: 
+Напишите на C# библиотеку для поставки внешним клиентам, которая умеет вычислять 
+площадь круга по радиусу и треугольника по трем сторонам. 
+Дополнительно к работоспособности оценим:
+  Юнит-тесты
+  Легкость добавления других фигур
+  Вычисление площади фигуры без знания типа фигуры в compile-time
+  Проверку на то, является ли треугольник прямоугольным
+
+---------------------------------------------------------------------------------
+РАЗБОР 18-й ЗАДАЧИ от С. КАМЯНЕЦКОГО
+// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+// x  y 
+// f  f
+// f  t
+// t  f
+// t  t
+
+// bool 
+// x = false , y = false;
+// Console.WriteLine(!(x || y) == (!x && !y));
+// x = false; y = true;
+// Console.WriteLine(!(x || y) == (!x && !y));
+// x = true; y = false;
+// Console.WriteLine(!(x || y) == (!x && !y));
+// x = true; y = true;
+// Console.WriteLine(!(x || y) == (!x && !y));
+
+bool f(bool arg1, bool arg2)
+{
+    return !(arg1 || arg2) == (!arg1 && !arg2);
+}
+
+// bool 
+// x = false , y = false;
+// Console.WriteLine(f(x, y));
+// x = false; y = true;
+// Console.WriteLine(f(x, y));
+// x = true; y = false;
+// Console.WriteLine(f(x, y));
+// x = true; y = true;
+// Console.WriteLine(f(x, y));
+
+// bool 
+// x = false , y = false;
+// bool res = f(x, y);
+
+// x = false; y = true;
+// res =res && f(x, y);
+// x = true; y = false;
+// res =res && f(x, y);
+// x = true; y = true;
+// res =res && f(x, y);
+// System.Console.WriteLine(res);
+
+bool ConvertIntToBool(int arg)
+{
+    return arg == 1;
+}
+// bool res = true;
+// for (int i = 0; i <= 1; i++)
+// {
+//     for (int j = 0; j <= 1; j++)
+//     {
+//         res = res && f(ConvertIntToBool(i), ConvertIntToBool(j));
+//     }
+// }
+//System.Console.WriteLine(res);
+
+bool Table()
+{
+    bool res = true;
+    for (int i = 0; i <= 1; i++)
+    {
+        for (int j = 0; j <= 1; j++)
+        {
+            res = res && f(Convert.ToBoolean(i), ConvertIntToBool(j));
+        }
+    }
+    return res;
+}
+
+Console.WriteLine(Table());
+
+
+*/
+
+// 16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+bool Zadanie16(int day)
+{
+    return day == 6 || day == 7;
+}
+
+
+//Console.WriteLine($"{(Zadanie16(7) ? "выходной" : "рабочий")}");
+
+//19. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+int Zadanie19(int x, int y)
+{
+    /*
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    return 4;
+    */
+
+    int a = (5==5) ? 4 : 6;
+    Console.WriteLine(a);
+
+    return (x > 0 && y > 0) ? 1 : ( (x < 0 && y > 0) ? 2 : ((x < 0 && y < 0) ? 3 :4 ) ); 
+}    
+
+int x = 5, y = -4;
+Console.WriteLine($"Номер четверти для x={x}, y={y} : {Zadanie19(x, y)}");
---
+++ middle/Program.cs
@@ -0,0 +1,230 @@
+﻿/*
+Семинар 11/11/2021
+
+Почувствуй себя мидлом
+23. Показать таблицу квадратов чисел от 1 до N 
+24. Найти кубы чисел от 1 до N
+25. Найти сумму чисел от 1 до А
+26. Возведите число А в натуральную степень B используя цикл
+27. Определить количество цифр в числе
+28. Подсчитать сумму цифр в числе
+29. Написать программу вычисления произведения чисел от 1 до N
+30. Показать кубы чисел, заканчивающихся на четную цифру
+
+*/
+
+// 23. Показать таблицу квадратов чисел от 1 до N 
+int f(int n, int stepen)
+{
+    return (int)Math.Pow(n, stepen);
+}
+
+string Zadanie23(int n)
+{
+    string result = String.Empty;
+    for (int i = 1; i <= n; i++)
+    {
+        result += $"{f(i, 2)} ";
+    }
+    return result;
+}
+
+void unitTestZadanie23()
+{
+    int n = 10;
+    Console.WriteLine(Zadanie23(n));
+}
+
+// 24. Найти кубы чисел от 1 до N
+string Zadanie24(int n)
+{
+    string result = String.Empty;
+
+    for (int i = 1; i <= n; i++)
+        result += $"{f(i, 3)} ";
+
+    return result;
+}
+
+
+void unitTestZadanie24()
+{
+    int n = 10;
+    Console.WriteLine(Zadanie24(n));
+}
+
+// 25. Найти сумму чисел от 1 до А
+(int, int) Zadanie25(int a)
+{
+
+    // result1 - Классичесский подсчет через цикл
+    int result1 = 0;
+    for (int i = 1; i <= a; i++)
+        result1 += i;
+
+    // result2 - через формулу арифметической прогрессии с коэффициентом 1
+    // sum=(A1+An)*n/2;
+    int result2 = (1 + a) * a / 2;
+
+    // возвращаем через кортеж, чтобы убедиться, что оба результата совпадают
+    return (result1, result2);
+}
+
+void unitTestZadanie25()
+{
+    int a = 100;
+    (int, int) res = Zadanie25(a);
+    Console.WriteLine($"Для A={a}: res1={res.Item1}, res2={res.Item2}");
+}
+
+// 26. Возведите число А в натуральную степень B используя цикл
+int Zadanie26(int a, int b)
+{
+    int result = a;
+    for (int i = 1; i < b; i++)
+        result *= a;
+
+    return result;
+}
+
+void unitTestZadanie26()
+{
+    int a = -3,
+        b = 5;
+
+    Console.WriteLine($"Для A = {a}, B = {b} Результат ({a})^{b} = {Zadanie26(a, b)}");
+}
+
+// 27. Определить количество цифр в числе
+int[] schetCifr(int n)
+{
+    int[] result = new int[10];
+    int k = Math.Abs(n);
+    while (k != 0)
+    {
+        int i = k % 10;
+        result[i]++;
+        k /= 10;
+    }
+
+    return result;
+}
+
+(int kol, int unic) Zadanie27(int n)
+{
+    int[] cifry = schetCifr(n);
+    int kolResult = 0;
+    int unicResult = 0;
+
+    // через foreach
+
+    foreach (int num in cifry)
+    {
+        kolResult += num;
+        if (num != 0)
+            unicResult++;
+    }
+
+
+    // через for
+    /*
+    for (int i = 0; i < cifry.Length; i++)
+    {
+        kolResult += cifry[i];
+        if (cifry[i] != 0)
+            unicResult++;
+    }
+    */
+
+    return (kolResult, unicResult);
+}
+
+void unitTestZadanie27()
+{
+    int n = -4506080;
+    (int, int) result = Zadanie27(n);
+    Console.WriteLine($"Число: {n}    Количество цифр: {result.Item1} Количество уникальных цифр: {result.Item2}");
+}
+
+// 28. Подсчитать сумму цифр в числе
+int Zadanie28(int n)
+{
+    int[] cifry = schetCifr(n);
+    int sum = 0;
+
+    for (int i = 0; i < cifry.Length; i++)
+        sum += i * cifry[i];
+
+    return sum;
+}
+
+void unitTestZadanie28()
+{
+    int n = -456550;
+
+    Console.WriteLine($"Число: {n}    Сумма цифр: {Zadanie28(n)}");
+}
+
+// 29. Написать программу вычисления произведения чисел от 1 до N
+ulong factorial(uint n)
+{
+    if (n <= 0) return 1;
+
+    return n * factorial(n - 1);
+}
+
+ulong Zadanie29(uint n)
+{
+    ulong result = 1;
+    for (uint i = 2; i <= n; i++)
+        result *= i;
+
+    return result;
+
+}
+
+void unitTestZadanie29()
+{
+    Console.Write("Введите n: ");
+    uint n = uint.Parse(Console.ReadLine()!);
+
+    Console.WriteLine($"Произведение чисел от 1 до {n} = {Zadanie29(n)}   Через рекурсию = {factorial(n)}");
+}
+
+// 30. Показать кубы чисел, заканчивающихся на четную цифру
+// решил сделать от -n до n и условие как относящееся к вычисляемым кубам, а не просто к числам
+string Zadanie30(int n)
+{
+    string result = string.Empty;
+    for (int i = -n; i <= n; i++)
+    {
+        int k = (int)Math.Pow(i, 3);
+        if (k % 2 == 0)
+            result += $"{i}: {k}\n\r";
+    }
+    return result;
+}
+
+
+
+void unitTestZadanie30()
+{
+    int n;
+    do
+    {
+        Console.Write("Введите n( >=0 ): ");
+        n = int.Parse(Console.ReadLine()!);
+    } while (n < 0);
+    Console.WriteLine(Zadanie30(n));
+}
+
+// ----------------------------
+// unitTestZadanie23();
+// unitTestZadanie24();
+// unitTestZadanie25();
+// unitTestZadanie26();
+// unitTestZadanie27();
+// unitTestZadanie28();
+// unitTestZadanie29();
+unitTestZadanie30();
+
